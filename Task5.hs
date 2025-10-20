62549117091e3bfd28c4c38f1eb9fc5225c9b74b
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-20 16:51:03 +0200

    Create Task5.hs

---
+++ Task5.hs
@@ -0,0 +1,12 @@
+-- infiniteNumbers: infinite list of natural numbers starting from 1
+infiniteNumbers :: [Integer]
+infiniteNumbers = [1..]
+
+-- takeN: take first n elements from infiniteNumbers
+takeN :: Int -> [Integer]
+takeN n = take n infiniteNumbers
+
+main :: IO ()
+main = do
+    print $ takeN 10
+    -- Output: [1,2,3,4,5,6,7,8,9,10]
