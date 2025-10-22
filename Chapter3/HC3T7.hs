619bca850621b336412b75cd51fa205b354561ed
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-22 21:33:30 +0200

    Update HC3T7.hs

--- Chapter3/HC3T7.hs
+++ Chapter3/HC3T7.hs
@@ -1,1 +1,14 @@
+season :: Int -> String
+season month
+    | month == 12 || month == 1 || month == 2  = "Winter"
+    | month >= 3 && month <= 5                 = "Spring"
+    | month >= 6 && month <= 8                 = "Summer"
+    | month >= 9 && month <= 11                = "Autumn"
+    | otherwise                                = "Invalid month"
+
+main :: IO ()
+main = do
+    print (season 3)   -- "Spring"
+    print (season 7)   -- "Summer"
+    print (season 11)  -- "Autumn"
 
