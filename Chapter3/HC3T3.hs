bb5eb4674561af3971f6d5acdd24cb45d7cc7a02
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-22 21:19:22 +0200

    Update HC3T3.hs

--- Chapter3/HC3T3.hs
+++ Chapter3/HC3T3.hs
@@ -1,1 +1,14 @@
+import Text.Printf (printf)
+
+rgbToHex :: (Int, Int, Int) -> String
+rgbToHex (r, g, b) =
+    let rHex = printf "%02X" r
+        gHex = printf "%02X" g
+        bHex = printf "%02X" b
+    in rHex ++ gHex ++ bHex
+
+main :: IO ()
+main = do
+    print (rgbToHex (255, 0, 127))   -- "FF007F"
+    print (rgbToHex (0, 255, 64))    -- "00FF40"
 
