e14a3e130e718593a1bc60e9b80694fc0446a643
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-22 21:36:10 +0200

    Update HC3T9.hs

--- Chapter3/HC3T9.hs
+++ Chapter3/HC3T9.hs
@@ -1,1 +1,11 @@
+maxOfThree :: Int -> Int -> Int -> Int
+maxOfThree x y z =
+    let maxXY = if x > y then x else y
+        maxXYZ = if maxXY > z then maxXY else z
+    in maxXYZ
+
+main :: IO ()
+main = do
+    print (maxOfThree 10 20 15)  -- 20
+    print (maxOfThree 5 25 10)   -- 25
 
