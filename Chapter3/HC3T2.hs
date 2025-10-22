a49cfeee99e7f7b096d919e9cfad17ddcf119e92
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-22 21:18:16 +0200

    Update HC3T2.hs

--- Chapter3/HC3T2.hs
+++ Chapter3/HC3T2.hs
@@ -1,1 +1,16 @@
+-- Main.hs
+
+grade :: Int -> String
+grade score
+    | score >= 90 = "A"
+    | score >= 80 = "B"
+    | score >= 70 = "C"
+    | score >= 60 = "D"
+    | otherwise   = "F"
+
+main :: IO ()
+main = do
+    print (grade 95)  -- "A"
+    print (grade 72)  -- "C"
+    print (grade 50)  -- "F"
 
