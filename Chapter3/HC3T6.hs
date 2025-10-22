f593388aef1fc9b171a121435294110caee532d3
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-22 21:32:10 +0200

    Update HC3T6.hs

--- Chapter3/HC3T6.hs
+++ Chapter3/HC3T6.hs
@@ -1,1 +1,13 @@
+isLeapYear :: Int -> Bool
+isLeapYear year =
+    if year `mod` 400 == 0 then True
+    else if year `mod` 100 == 0 then False
+    else if year `mod` 4 == 0 then True
+    else False
+
+main :: IO ()
+main = do
+    print (isLeapYear 2000)  -- True
+    print (isLeapYear 1900)  -- False
+    print (isLeapYear 2024)  -- True
 
