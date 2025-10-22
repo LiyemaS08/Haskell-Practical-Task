540b32d44748459efe1f9c17b8432d281f15ccc7
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-22 21:37:20 +0200

    Update HC3T10.hs

--- Chapter3/HC3T10.hs
+++ Chapter3/HC3T10.hs
@@ -1,1 +1,12 @@
+isPalindrome :: String -> Bool
+isPalindrome s
+    | length s <= 1                   = True
+    | head s == last s               = isPalindrome (init (tail s))
+    | otherwise                     = False
+
+main :: IO ()
+main = do
+    print (isPalindrome "racecar")  -- True
+    print (isPalindrome "haskell")  -- False
+    print (isPalindrome "madam")    -- True
 
