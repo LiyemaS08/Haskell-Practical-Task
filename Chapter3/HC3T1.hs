a5ffc0484687214f48f3807fb8236e74f170eb70
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-22 21:11:50 +0200

    Update HC3T1.hs

--- Chapter3/HC3T1.hs
+++ Chapter3/HC3T1.hs
@@ -1,1 +1,14 @@
+-- Main.hs
+
+checkNumber :: Int -> String
+checkNumber n = 
+    if n > 0 then "Positive"
+    else if n < 0 then "Negative"
+    else "Zero"
+
+main :: IO ()
+main = do
+    print (checkNumber 5)    -- Should print "Positive"
+    print (checkNumber (-3)) -- Should print "Negative"
+    print (checkNumber 0)    -- Should print "Zero"
 
