71bead4c233f17036b718af6b3c58832162182b1
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-22 21:31:01 +0200

    Update HC3T5.hs

--- Chapter3/HC3T5.hs
+++ Chapter3/HC3T5.hs
@@ -1,1 +1,12 @@
+triangleType :: Float -> Float -> Float -> String
+triangleType a b c
+    | a == b && b == c           = "Equilateral"
+    | a == b || b == c || a == c = "Isosceles"
+    | otherwise                  = "Scalene"
+
+main :: IO ()
+main = do
+    print (triangleType 3 3 3)  -- "Equilateral"
+    print (triangleType 5 5 8)  -- "Isosceles"
+    print (triangleType 6 7 8)  -- "Scalene"
 
