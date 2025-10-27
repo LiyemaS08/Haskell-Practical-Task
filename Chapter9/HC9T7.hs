073ce7720b02bfe0eae6710792a689c67f649fe4
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-27 21:11:05 +0200

    Update HC9T7.hs

--- Chapter9/HC9T7.hs
+++ Chapter9/HC9T7.hs
@@ -1,1 +1,18 @@
+-- HC9T7.hs
+data Tweet = Tweet
+  { content :: String
+  , likes :: Int
+  , comments :: [Tweet]
+  } deriving (Show)
+
+engagement :: Tweet -> Int
+engagement (Tweet _ likes comments) =
+    likes + sum (map engagement comments)
+
+main :: IO ()
+main = do
+    let c1 = Tweet "Nice!" 3 []
+    let c2 = Tweet "Cool!" 5 []
+    let post = Tweet "My first tweet!" 10 [c1, c2]
+    print (engagement post)  -- 18
 
