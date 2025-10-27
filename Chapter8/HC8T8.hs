e3c39d0169e03fa0816bb9b3aae8248011f210f6
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-27 20:54:21 +0200

    Update HC8T8.hs

--- Chapter8/HC8T8.hs
+++ Chapter8/HC8T8.hs
@@ -1,1 +1,10 @@
+-- HC8T8.hs
+type Name = String
+type Age = Int
+
+greet :: Name -> Age -> String
+greet name age = "Hello, " ++ name ++ "! You are " ++ show age ++ " years old."
+
+main :: IO ()
+main = putStrLn (greet "Amahle" 25)
 
