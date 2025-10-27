79a3dee45c1b4d2dc8fcdf835972c87408d7d253
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-27 21:12:56 +0200

    Update HC9T10.hs

--- Chapter9/HC9T10.hs
+++ Chapter9/HC9T10.hs
@@ -1,1 +1,11 @@
+-- HC9T10.hs
+data BST a
+  = Empty
+  | Node a (BST a) (BST a)
+  deriving (Show)
+
+main :: IO ()
+main = do
+    let tree = Node 10 (Node 5 Empty Empty) (Node 15 Empty Empty)
+    print tree
 
