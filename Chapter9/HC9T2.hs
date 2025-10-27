19d360c07f39673a221f39161560a60c155b56f1
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-27 21:06:09 +0200

    Update HC9T2.hs

--- Chapter9/HC9T2.hs
+++ Chapter9/HC9T2.hs
@@ -1,1 +1,13 @@
+-- HC9T2.hs
+data Box a = Empty | Has a deriving (Show)
+
+main :: IO ()
+main = do
+    let b1 :: Box Int  -- specify the type explicitly
+        b1 = Empty
+
+    let b2 = Has 10
+
+    print b1
+    print b2
 
