ac40fcc4fd89af873bef90b7f89d7629165d6f51
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-27 21:10:31 +0200

    Update HC9T6.hs

--- Chapter9/HC9T6.hs
+++ Chapter9/HC9T6.hs
@@ -1,1 +1,13 @@
+-- HC9T6.hs
+data Tweet = Tweet
+  { content :: String
+  , likes :: Int
+  , comments :: [Tweet]
+  } deriving (Show)
+
+main :: IO ()
+main = do
+    let reply = Tweet "Nice post!" 2 []
+    let post = Tweet "Hello world!" 10 [reply]
+    print post
 
