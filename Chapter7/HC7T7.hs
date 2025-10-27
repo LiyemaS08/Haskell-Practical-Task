e516d65b5489883501b8a3258026e60bf1285ded
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-27 20:40:06 +0200

    Update HC7T7.hs

--- Chapter7/HC7T7.hs
+++ Chapter7/HC7T7.hs
@@ -1,1 +1,14 @@
+-- HC7T7.hs
+data Color = Red | Green | Blue
+    deriving (Show, Eq, Enum, Bounded)
+
+nextColor :: Color -> Color
+nextColor c
+    | c == maxBound = minBound
+    | otherwise     = succ c
+
+main :: IO ()
+main = do
+    print (nextColor Red)   -- Green
+    print (nextColor Blue)  -- Red
 
