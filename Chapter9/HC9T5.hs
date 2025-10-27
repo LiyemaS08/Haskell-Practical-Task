459fc05095ea7325871ec3a29c1f3a8192cbed6f
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-27 21:08:46 +0200

    Update HC9T5.hs

--- Chapter9/HC9T5.hs
+++ Chapter9/HC9T5.hs
@@ -1,1 +1,13 @@
+-- HC9T5.hs
+data Shape a
+  = Circle { color :: a, radius :: Float }
+  | Rectangle { color :: a, width :: Float, height :: Float }
+  deriving (Show)
+
+main :: IO ()
+main = do
+    let s1 = Circle { color = "Red", radius = 5.0 }
+    let s2 = Rectangle { color = "Blue", width = 10.0, height = 4.0 }
+    print s1
+    print s2
 
