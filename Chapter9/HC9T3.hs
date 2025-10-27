f3db1e40f759820a5c15ab5d545662d8353c6f0b
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-27 21:06:55 +0200

    Update HC9T3.hs

--- Chapter9/HC9T3.hs
+++ Chapter9/HC9T3.hs
@@ -1,1 +1,12 @@
+-- HC9T3.hs
+data Box a = Empty | Has a deriving (Show)
+
+addN :: Num a => a -> Box a -> Box a
+addN n Empty = Empty
+addN n (Has x) = Has (x + n)
+
+main :: IO ()
+main = do
+    print (addN 5 (Has 10))  -- Has 15
+    print (addN 3 Empty)     -- Empty
 
