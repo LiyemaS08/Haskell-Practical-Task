0bfde756da218290e487621e1a3f8f4b6d737920
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-27 20:49:37 +0200

    Update HC8T7.hs

--- Chapter8/HC8T7.hs
+++ Chapter8/HC8T7.hs
@@ -1,1 +1,14 @@
+-- HC8T7.hs
+data Animal = Dog String | Cat String deriving (Show)
 
+describeAnimal :: Animal -> String
+describeAnimal (Dog name) = "This is a dog named " ++ name
+describeAnimal (Cat name) = "This is a cat named " ++ name
+
+main :: IO ()
+main = do
+    let dog1 = Dog "Buddy"
+    let cat1 = Cat "Whiskers"
+    putStrLn (describeAnimal dog1)
+    putStrLn (describeAnimal cat1)
+v
