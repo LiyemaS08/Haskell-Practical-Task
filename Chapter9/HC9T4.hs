70b5f3be15616c31160ecaeea121193c372356f3
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-27 21:07:17 +0200

    Update HC9T4.hs

--- Chapter9/HC9T4.hs
+++ Chapter9/HC9T4.hs
@@ -1,1 +1,12 @@
+-- HC9T4.hs
+data Box a = Empty | Has a deriving (Show)
+
+extract :: a -> Box a -> a
+extract def Empty    = def
+extract _   (Has x)  = x
+
+main :: IO ()
+main = do
+    print (extract 0 (Has 10))   -- 10
+    print (extract 0 Empty)      -- 0
 
