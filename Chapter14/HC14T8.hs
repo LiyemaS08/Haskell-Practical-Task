658261e9d27e48f7c65a0bd572ba1b017d7255be
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:33:39 +0200

    Update HC14T8.hs

--- Chapter14/HC14T8.hs
+++ Chapter14/HC14T8.hs
@@ -1,1 +1,9 @@
+-- HC14T8
+import Data.List (group, sort)
+
+counts :: String -> [(Char, Int)]
+counts str = [(head g, length g) | g <- group (sort str)]
+
+main :: IO ()
+main = print (counts "haskell")
 
