62530fa8ac0cec796e7b425e049b0d6060fa285d
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:31:47 +0200

    Update HC14T5.hs

--- Chapter14/HC14T5.hs
+++ Chapter14/HC14T5.hs
@@ -1,1 +1,12 @@
+-- HC14T5
+data Result a = Success a | Failure String
+
+process :: Result Int -> String
+process r@(Success x) = "Success: " ++ show x ++ " (full pattern: " ++ show r ++ ")"
+process (Failure msg) = "Failure: " ++ msg
+
+main :: IO ()
+main = do
+    print (process (Success 10))
+    print (process (Failure "Something went wrong"))
 
