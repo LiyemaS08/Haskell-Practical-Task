bee0d165c659178b51fe4b8bcb6d694dfb698a34
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:46:22 +0200

    Update HC15T10.hs

--- Chapter15/HC15T10.hs
+++ Chapter15/HC15T10.hs
@@ -1,1 +1,21 @@
+-- HC15T10 - Hybrid Error Handling with Either and IO
+import Control.Exception
+import Text.Read (readMaybe)
+
+safeDivide :: Double -> Double -> Either String Double
+safeDivide _ 0 = Left "Cannot divide by zero."
+safeDivide x y = Right (x / y)
+
+main :: IO ()
+main = do
+    putStrLn "Enter distance (m):"
+    dStr <- getLine
+    putStrLn "Enter time (s):"
+    tStr <- getLine
+    case (readMaybe dStr, readMaybe tStr) of
+        (Just d, Just t) -> do
+            case safeDivide d t of
+                Left err -> putStrLn ("Error: " ++ err)
+                Right v  -> putStrLn ("Velocity = " ++ show v ++ " m/s")
+        _ -> putStrLn "Invalid numeric input."
 
