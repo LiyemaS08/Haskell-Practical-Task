068d6bc09e7998c47536af4f1596fe450aad2c4c
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:19:12 +0200

    Update HC13T5.hs

--- Chapter13/HC13T5.hs
+++ Chapter13/HC13T5.hs
@@ -1,1 +1,11 @@
+-- HC13T5 - Restricted Export
+module SumNonEmpty (sumNonEmpty) where
+
+sumNonEmpty :: [Int] -> Int
+sumNonEmpty [] = errorMsg
+sumNonEmpty xs = sum xs
+
+-- private helper (not exported)
+errorMsg :: String
+errorMsg = error "List cannot be empty!"
 
