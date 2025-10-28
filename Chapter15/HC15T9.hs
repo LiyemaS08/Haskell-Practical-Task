878bae27d7d589dc2c56600d1109b800aeb3fa52
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:45:52 +0200

    Update HC15T9.hs

--- Chapter15/HC15T9.hs
+++ Chapter15/HC15T9.hs
@@ -1,1 +1,13 @@
+-- HC15T9 - Try Function for File IO Exceptions
+import Control.Exception
+import System.IO
+
+main :: IO ()
+main = do
+    putStrLn "Enter filename:"
+    f <- getLine
+    result <- try (readFile f) :: IO (Either IOError String)
+    case result of
+        Left _  -> putStrLn "File could not be read."
+        Right c -> putStrLn ("File contents:\n" ++ c)
 
