d0db8038e448cbfe4fa1b571069538e2afe94110
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:44:03 +0200

    Update HC15T5.hs

--- Chapter15/HC15T5.hs
+++ Chapter15/HC15T5.hs
@@ -1,1 +1,12 @@
+-- HC15T5 - Safe Division Using Maybe
+safeDivide :: Double -> Double -> Maybe Double
+safeDivide _ 0 = Nothing
+safeDivide x y = Just (x / y)
+
+main :: IO ()
+main = do
+    let result = safeDivide 10 0
+    case result of
+        Nothing -> putStrLn "Cannot divide by zero."
+        Just v  -> putStrLn ("Result: " ++ show v)
 
