3bc055c980358dff4feabcdff7442a4e2835f5cd
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:20:37 +0200

    Update HC13T10.hs

--- Chapter13/HC13T10.hs
+++ Chapter13/HC13T10.hs
@@ -1,1 +1,12 @@
+-- HC13T10 - Multi-Module Main Function
+import System.Directory
+import Data.List (sort, isInfixOf)
+
+main :: IO ()
+main = do
+    putStrLn "Enter a keyword to search for files:"
+    key <- getLine
+    files <- listDirectory "."
+    let results = sort (filter (isInfixOf key) files)
+    mapM_ putStrLn results
 
