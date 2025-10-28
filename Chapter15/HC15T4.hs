1469e99066e6a9d437e5f4ccc2474ca2a2aedeb1
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:43:47 +0200

    Update HC15T4.hs

--- Chapter15/HC15T4.hs
+++ Chapter15/HC15T4.hs
@@ -1,1 +1,17 @@
+-- HC15T4 - Exception Handler for Traffic Light
+import Control.Exception
+
+data TrafficLightError = InvalidLight String deriving (Show)
+instance Exception TrafficLightError
+
+main :: IO ()
+main = handle handler $ do
+    putStrLn "Enter traffic light color:"
+    color <- getLine
+    if color `elem` ["red","yellow","green"]
+       then putStrLn ("Valid color: " ++ color)
+       else throwIO (InvalidLight color)
+  where
+    handler :: TrafficLightError -> IO ()
+    handler (InvalidLight c) = putStrLn ("Error: '" ++ c ++ "' is not a valid traffic light color.")
 
