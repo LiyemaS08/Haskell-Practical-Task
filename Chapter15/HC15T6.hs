3b26f5811f1a00868e99bb6206d1bd0b0b63df56
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:44:18 +0200

    Update HC15T6.hs

--- Chapter15/HC15T6.hs
+++ Chapter15/HC15T6.hs
@@ -1,1 +1,11 @@
+-- HC15T6 - Safe Input Parsing with readMaybe
+import Text.Read (readMaybe)
+
+main :: IO ()
+main = do
+    putStrLn "Enter an integer:"
+    input <- getLine
+    case readMaybe input :: Maybe Int of
+        Nothing -> putStrLn "Invalid number!"
+        Just n  -> putStrLn ("You entered: " ++ show n)
 
