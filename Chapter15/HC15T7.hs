97a162fe9446fba60ce847bd92c399f11315870e
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:44:36 +0200

    Update HC15T7.hs

--- Chapter15/HC15T7.hs
+++ Chapter15/HC15T7.hs
@@ -1,1 +1,16 @@
+-- HC15T7 - Velocity Calculation with Optionals and Parsing Handling
+import Text.Read (readMaybe)
+
+main :: IO ()
+main = do
+    putStrLn "Enter distance (m):"
+    dStr <- getLine
+    putStrLn "Enter time (s):"
+    tStr <- getLine
+    case (readMaybe dStr, readMaybe tStr) of
+        (Just d, Just t) ->
+            if t == 0
+                then putStrLn "Error: Time cannot be zero."
+                else putStrLn ("Velocity = " ++ show (d / t) ++ " m/s")
+        _ -> putStrLn "Invalid numeric input."
 
