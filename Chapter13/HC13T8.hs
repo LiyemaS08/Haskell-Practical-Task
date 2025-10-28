cecfa3bf9ea80ca34429a9e185e7a574a0c2e792
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:20:08 +0200

    Update HC13T8.hs

--- Chapter13/HC13T8.hs
+++ Chapter13/HC13T8.hs
@@ -1,1 +1,11 @@
+-- HC13T8 - Qualified Imports
+import qualified Data.List as L
+import qualified Data.Map as M
+
+main :: IO ()
+main = do
+    let sorted = L.sort [3,1,2]
+    let myMap = M.fromList [(1,"One"), (2,"Two")]
+    print sorted
+    print (M.lookup 2 myMap)
 
