9952746a35dabba452759269a77f4c48fef60d19
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:18:06 +0200

    Update HC13T1.hs

--- Chapter13/HC13T1.hs
+++ Chapter13/HC13T1.hs
@@ -1,1 +1,9 @@
+-- HC13T1 - List Files in Directory
+import System.Directory
+
+main :: IO ()
+main = do
+    putStrLn "Files in current directory:"
+    files <- listDirectory "."
+    mapM_ putStrLn files
 
