dfb247c2b4b25b94ade04c03548304e418bcf1c0
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:18:36 +0200

    Update HC13T3.hs

--- Chapter13/HC13T3.hs
+++ Chapter13/HC13T3.hs
@@ -1,1 +1,13 @@
+-- HC13T3 - Sort and Return Filtered Files
+import System.Directory
+import Data.List (sort, isInfixOf)
+
+main :: IO ()
+main = do
+    putStrLn "Enter substring to filter:"
+    keyword <- getLine
+    files <- listDirectory "."
+    let filtered = filter (isInfixOf keyword) files
+    putStrLn "Sorted filtered files:"
+    mapM_ putStrLn (sort filtered)
 
