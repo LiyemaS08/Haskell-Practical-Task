6bfcf75e9a4d77d69eb7f24f018200c85aba8081
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:45:37 +0200

    Update HC15T8.hs

--- Chapter15/HC15T8.hs
+++ Chapter15/HC15T8.hs
@@ -1,1 +1,12 @@
+-- HC15T8 - Division with Either for Detailed Errors
+safeDivide :: Double -> Double -> Either String Double
+safeDivide _ 0 = Left "Cannot divide by zero."
+safeDivide x y = Right (x / y)
+
+main :: IO ()
+main = do
+    let res = safeDivide 10 0
+    case res of
+        Left err -> putStrLn ("Error: " ++ err)
+        Right val -> putStrLn ("Result: " ++ show val)
 
