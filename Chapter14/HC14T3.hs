652f378672944d7d260133ff4b42a8c35514bf92
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:31:12 +0200

    Update HC14T3.hs

--- Chapter14/HC14T3.hs
+++ Chapter14/HC14T3.hs
@@ -1,1 +1,10 @@
+{-# LANGUAGE NumericUnderscores #-}
+
+-- HC14T3
+main :: IO ()
+main = do
+    let bigNumber = 1_000_000
+    let population = 8_200_000_000
+    print bigNumber
+    print population
 
