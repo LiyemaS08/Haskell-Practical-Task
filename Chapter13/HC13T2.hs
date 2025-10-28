90906a7af3b54476c61bf2368503ff352417dc9b
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:18:23 +0200

    Update HC13T2.hs

--- Chapter13/HC13T2.hs
+++ Chapter13/HC13T2.hs
@@ -1,1 +1,13 @@
+-- HC13T2 - Filter Files by Substring
+import System.Directory
+import Data.List (isInfixOf)
+
+main :: IO ()
+main = do
+    putStrLn "Enter a substring to filter filenames:"
+    keyword <- getLine
+    files <- listDirectory "."
+    let matched = filter (isInfixOf keyword) files
+    putStrLn "Matching files:"
+    mapM_ putStrLn matched
 
