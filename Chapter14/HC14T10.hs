375504c48336f29a5ea5af2a463dc2cd59e3390a
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:34:18 +0200

    Update HC14T10.hs

--- Chapter14/HC14T10.hs
+++ Chapter14/HC14T10.hs
@@ -1,1 +1,9 @@
+import Test.HUnit
+import Counts
+
+main :: IO ()
+main = do
+    let test1 = TestCase (assertEqual "Count test" [('a',2)] (counts "aa"))
+    _ <- runTestTT (TestList [test1])
+    return ()
 
