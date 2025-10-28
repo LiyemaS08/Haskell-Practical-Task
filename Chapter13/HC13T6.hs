cd9911f41470137c495f643aa9b2e38c5cf17cb5
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:19:31 +0200

    Update HC13T6.hs

--- Chapter13/HC13T6.hs
+++ Chapter13/HC13T6.hs
@@ -1,1 +1,10 @@
+-- HC13T6 - File Names to Map
+import System.Directory
+import qualified Data.Map as Map
+
+main :: IO ()
+main = do
+    files <- listDirectory "."
+    let fileMap = Map.fromList (zip [1..] files)
+    print fileMap
 
