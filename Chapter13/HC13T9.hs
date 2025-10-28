012ff48c05e4641e9438571850b3e3b30ef27e22
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-28 22:20:22 +0200

    Update HC13T9.hs

--- Chapter13/HC13T9.hs
+++ Chapter13/HC13T9.hs
@@ -1,1 +1,10 @@
+-- HC13T9 - Renaming Module Namespace
+import qualified Data.List as ListModule
+import qualified Data.Char as C
+
+main :: IO ()
+main = do
+    let word = "Haskell"
+    print (ListModule.sort word)
+    print (map C.toUpper word)
 
