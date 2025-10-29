e3fa14169c877418086caf459aef3a94af24c29f
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 17:19:03 +0200

    Update HC21T18.hs

--- Chapter21/HC21T18.hs
+++ Chapter21/HC21T18.hs
@@ -1,8 +1,1 @@
-module Main where
-
-combineEitherResults :: Either String Int -> Either String Int -> Either String Int -> Either String Int
-combineEitherResults a b c = (\x y z -> x + y + z) <$> a <*> b <*> c
-
-main :: IO ()
-main = print (combineEitherResults (Right 1) (Right 2) (Right 3))
 
