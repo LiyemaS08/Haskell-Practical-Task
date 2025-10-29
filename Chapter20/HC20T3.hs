bc6521c39cd580f56b65ae497ad676ed12e09613
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:32:31 +0200

    Update HC20T3.hs

--- Chapter20/HC20T3.hs
+++ Chapter20/HC20T3.hs
@@ -1,1 +1,22 @@
+module Main where
+
+import Control.Monad.Writer
+
+add :: Int -> Int -> Writer [String] Int
+add x y = tell ["Adding " ++ show x ++ " and " ++ show y] >> return (x + y)
+
+multiply :: Int -> Int -> Writer [String] Int
+multiply x y = tell ["Multiplying " ++ show x ++ " and " ++ show y] >> return (x * y)
+
+calculator :: Writer [String] Int
+calculator = do
+  a <- add 3 5
+  b <- multiply a 2
+  return b
+
+main :: IO ()
+main = do
+  let (result, logMsgs) = runWriter calculator
+  print result
+  mapM_ putStrLn logMsgs
 
