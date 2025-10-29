44dcd97bf2d3dbd7cbd3097b9f65971c482e207c
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:23:30 +0200

    Update HC19T6.hs

--- Chapter19/HC19T6.hs
+++ Chapter19/HC19T6.hs
@@ -1,1 +1,10 @@
+module Main where
+
+import Control.Monad (forever)
+
+repeatEffect :: IO () -> IO ()
+repeatEffect action = forever action
+
+main :: IO ()
+main = repeatEffect (putStrLn "Repeating forever...")  -- Ctrl+C to stop
 
