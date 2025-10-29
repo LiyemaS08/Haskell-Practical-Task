ed09096f158579b3a1874603b7813a641d8d7af3
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:12:11 +0200

    Update HC21T5.hs

--- Chapter21/HC21T5.hs
+++ Chapter21/HC21T5.hs
@@ -1,1 +1,10 @@
+module Main where
+
+applyEffects :: (IO Int, IO Int) -> IO Int
+applyEffects (a, b) = (+) <$> a <*> b
+
+main :: IO ()
+main = do
+  result <- applyEffects (print 3 >> return 3, print 4 >> return 4)
+  putStrLn ("Sum: " ++ show result)
 
