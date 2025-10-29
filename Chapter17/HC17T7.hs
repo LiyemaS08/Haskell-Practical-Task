a3f921aea94af9995e65a32494b6bc313274c020
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 13:54:07 +0200

    Update HC17T7.hs

--- Chapter17/HC17T7.hs
+++ Chapter17/HC17T7.hs
@@ -1,1 +1,17 @@
+-- HC17T7: multiplyProducts Function
+newtype Product = Product { getProduct :: Int } deriving (Show, Eq)
+
+instance Semigroup Product where
+    (Product x) <> (Product y) = Product (x * y)
+
+instance Monoid Product where
+    mempty = Product 1
+
+multiplyProducts :: [Product] -> Product
+multiplyProducts = mconcat
+
+main :: IO ()
+main = do
+    putStrLn "HC17T7: multiplyProducts"
+    print (multiplyProducts [Product 2, Product 3, Product 4]) -- Product 24
 
