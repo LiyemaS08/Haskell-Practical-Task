5a8fcbc57a05df7e7b5e7bf1512947c91a0806ce
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:15:51 +0200

    Update HC21T11.hs

--- Chapter21/HC21T11.hs
+++ Chapter21/HC21T11.hs
@@ -1,1 +1,14 @@
+module Main where
+
+data Wrapper a = Wrapper a deriving (Show, Eq)
+
+instance Functor Wrapper where
+  fmap f (Wrapper x) = Wrapper (f x)
+
+instance Applicative Wrapper where
+  pure = Wrapper
+  Wrapper f <*> Wrapper x = Wrapper (f x)
+
+main :: IO ()
+main = print (Wrapper (+1) <*> Wrapper 5)
 
