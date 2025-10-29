d1f74a44550d4e93965da7421f6df22c225848d1
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:35:19 +0200

    Update HC20T10.hs

--- Chapter20/HC20T10.hs
+++ Chapter20/HC20T10.hs
@@ -1,1 +1,17 @@
+module Main where
+
+import Control.Monad.State
+import Control.Monad.Trans.Maybe
+
+type MyState = StateT Int (MaybeT IO)
+
+incrementIfEven :: MyState ()
+incrementIfEven = do
+  n <- get
+  if even n then put (n + 1) else lift (MaybeT (return Nothing))
+
+main :: IO ()
+main = do
+  result <- runMaybeT (runStateT incrementIfEven 4)
+  print result
 
