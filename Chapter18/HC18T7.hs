00b833d3aaaf59fbbd2931e25f76d03c1dec93a0
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 14:04:04 +0200

    Update HC18T7.hs

--- Chapter18/HC18T7.hs
+++ Chapter18/HC18T7.hs
@@ -1,1 +1,9 @@
+-- HC18T7: fmapTuple Function
+fmapTuple :: (b -> c) -> (a, b) -> (a, c)
+fmapTuple = fmap
+
+main :: IO ()
+main = do
+    putStrLn "HC18T7: fmapTuple"
+    print (fmapTuple length ("Count", "Hello"))  -- ("Count",5)
 
