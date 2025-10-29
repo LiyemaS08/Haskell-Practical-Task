57f1e4f97a3fa6ce8e7008e7090079be6f26e4d2
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 14:02:59 +0200

    Update HC18T5.hs

--- Chapter18/HC18T5.hs
+++ Chapter18/HC18T5.hs
@@ -1,1 +1,14 @@
+-- HC18T5: Functor Instance for Either
+data Either' a b = Left' a | Right' b
+  deriving (Show)
+
+instance Functor (Either' a) where
+    fmap _ (Left' x)  = Left' x
+    fmap f (Right' y) = Right' (f y)
+
+main :: IO ()
+main = do
+    putStrLn "HC18T5: Functor Instance for Either"
+    print (fmap (*2) (Right' 5 :: Either' String Int))  -- Right' 10
+    print (fmap (*2) (Left' "Error" :: Either' String Int))  -- Left' "Error"
 
