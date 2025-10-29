c3d8b354b7fd0f80c467356d56688c791e50d32d
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 13:53:45 +0200

    Update HC17T6.hs

--- Chapter17/HC17T6.hs
+++ Chapter17/HC17T6.hs
@@ -1,1 +1,19 @@
+-- HC17T6: maxSeverity Function
+data Severity = Low | Medium | High | Critical
+  deriving (Show, Eq, Ord)
+
+instance Semigroup Severity where
+    (<>) = max
+
+instance Monoid Severity where
+    mempty = Low
+
+maxSeverity :: [Severity] -> Severity
+maxSeverity = mconcat
+
+main :: IO ()
+main = do
+    putStrLn "HC17T6: maxSeverity"
+    print (maxSeverity [Low, Medium, High])       -- High
+    print (maxSeverity [Low, Critical, Medium])   -- Critical
 
