46c92b2d43de7f2507a9b57932bee85bcd144e8d
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:11:38 +0200

    Update HC21T4.hs

--- Chapter21/HC21T4.hs
+++ Chapter21/HC21T4.hs
@@ -1,1 +1,10 @@
+module Main where
+
+import Control.Applicative (liftA2)
+
+liftAndMultiply :: Maybe Int -> Maybe Int -> Maybe Int
+liftAndMultiply = liftA2 (*)
+
+main :: IO ()
+main = print (liftAndMultiply (Just 3) (Just 4))
 
