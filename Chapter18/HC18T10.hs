ad8f4be360dee02def2ecf18b7fd63976cdeda67
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 14:08:24 +0200

    Update HC18T10.hs

--- Chapter18/HC18T10.hs
+++ Chapter18/HC18T10.hs
@@ -1,1 +1,9 @@
+-- HC18T10: nestedFmap Function
+nestedFmap :: (a -> b) -> [[a]] -> [[b]]
+nestedFmap = fmap . fmap
+
+main :: IO ()
+main = do
+    putStrLn "HC18T10: nestedFmap"
+    print (nestedFmap (*2) [[1,2],[3,4]])  -- [[2,4],[6,8]]
 
