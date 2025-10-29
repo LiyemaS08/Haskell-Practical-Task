c23b008c86ebcbf59998a7c29e15ed53ffdae048
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:15:19 +0200

    Update HC21T10.hs

--- Chapter21/HC21T10.hs
+++ Chapter21/HC21T10.hs
@@ -1,1 +1,10 @@
+module Main where
+
+import Control.Applicative (liftA2)
+
+combineResults :: Either String Int -> Either String Int -> Either String Int
+combineResults = liftA2 (+)
+
+main :: IO ()
+main = print (combineResults (Right 3) (Right 4))
 
