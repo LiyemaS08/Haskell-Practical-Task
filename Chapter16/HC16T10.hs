7b59fe3b9adf4e8d18415e5dd46a5ea7e6b471c0
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 11:27:58 +0200

    Update HC16T10.hs

--- Chapter16/HC16T10.hs
+++ Chapter16/HC16T10.hs
@@ -1,1 +1,13 @@
+-- HC16T10: Character Frequency in String
+import Data.List (nub)
+
+charFrequency :: String -> [(Char, Int)]
+charFrequency str = [(c, count c str) | c <- nub str]
+  where
+    count ch xs = length (filter (== ch) xs)
+
+main :: IO ()
+main = do
+    putStrLn "HC16T10: Character Frequency in String"
+    print (charFrequency "haskell")
 
