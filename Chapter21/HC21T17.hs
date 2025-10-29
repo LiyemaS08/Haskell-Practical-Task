fa17c1cb8d77c46f7ee9d632c05e8c0f82284a1d
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:18:57 +0200

    Update HC21T17.hs

--- Chapter21/HC21T17.hs
+++ Chapter21/HC21T17.hs
@@ -1,1 +1,8 @@
+module Main where
+
+simulateMaybeEffect :: Maybe Int -> Maybe Int -> Maybe Int -> Maybe Int
+simulateMaybeEffect a b c = (\x y z -> x * y + z) <$> a <*> b <*> c
+
+main :: IO ()
+main = print (simulateMaybeEffect (Just 2) (Just 3) (Just 4))
 
