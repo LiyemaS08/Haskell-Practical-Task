302012de0e2a970a2bfde8a35dd0e1b6ca76c946
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 14:00:01 +0200

    Update HC18T1.hs

--- Chapter18/HC18T1.hs
+++ Chapter18/HC18T1.hs
@@ -1,1 +1,11 @@
+-- HC18T1: mapToLower Function with fmap
+import Data.Char (toLower)
+
+mapToLower :: String -> String
+mapToLower = fmap toLower
+
+main :: IO ()
+main = do
+    putStrLn "HC18T1: mapToLower"
+    print (mapToLower "HASKELL")  -- "haskell"
 
