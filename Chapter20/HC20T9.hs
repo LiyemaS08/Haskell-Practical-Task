1108b6920ceea4525ace1c5e8bf53fbac557f73f
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:35:04 +0200

    Update HC20T9.hs

--- Chapter20/HC20T9.hs
+++ Chapter20/HC20T9.hs
@@ -1,1 +1,10 @@
+module Main where
+
+import Data.Functor.Identity
+
+replicateMonad :: Int -> a -> Identity [a]
+replicateMonad n x = return (replicate n x)
+
+main :: IO ()
+main = print (runIdentity (replicateMonad 4 'z'))
 
