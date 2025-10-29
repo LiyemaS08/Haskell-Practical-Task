62da3a3b4897151dbc02cc6dbf724869ceb68566
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:48:04 +0200

    Update HC20T16.hs

--- Chapter20/HC20T16.hs
+++ Chapter20/HC20T16.hs
@@ -1,1 +1,17 @@
+module Main where
+
+retryIO :: Int -> IO a -> IO (Maybe a)
+retryIO 0 _ = return Nothing
+retryIO n action = do
+  result <- (Just <$> action) `catch` (\(_ :: IOError) -> return Nothing)
+  case result of
+    Just v  -> return (Just v)
+    Nothing -> retryIO (n - 1) action
+
+import Control.Exception (catch, IOException)
+
+main :: IO ()
+main = do
+  res <- retryIO 3 (readFile "missing.txt")
+  print (fmap (take 20) res)
 
