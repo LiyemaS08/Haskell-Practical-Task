c524b5c4c197b56d0b82efc13a7c44d9444342dd
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 13:54:36 +0200

    Update HC17T8.hs

--- Chapter17/HC17T8.hs
+++ Chapter17/HC17T8.hs
@@ -1,1 +1,9 @@
+-- HC17T8: foldWithSemigroup Function
+foldWithSemigroup :: Semigroup a => [a] -> a
+foldWithSemigroup = foldr1 (<>)
+
+main :: IO ()
+main = do
+    putStrLn "HC17T8: foldWithSemigroup"
+    print (foldWithSemigroup ["Hello", " ", "World"]) -- "Hello World"
 
