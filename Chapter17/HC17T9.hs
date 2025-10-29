eb69813a139177a20e0a36370758f5199223b01c
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 13:55:08 +0200

    Update HC17T9.hs

--- Chapter17/HC17T9.hs
+++ Chapter17/HC17T9.hs
@@ -1,1 +1,21 @@
+-- HC17T9: Config Data Type and Semigroup Instance
+data Config = Config
+  { loggingLevel :: Int
+  , timeout      :: Int
+  , retries      :: Int
+  } deriving (Show)
+
+instance Semigroup Config where
+    c1 <> c2 = Config
+        { loggingLevel = max (loggingLevel c1) (loggingLevel c2)
+        , timeout      = min (timeout c1) (timeout c2)
+        , retries      = max (retries c1) (retries c2)
+        }
+
+main :: IO ()
+main = do
+    putStrLn "HC17T9: Config Semigroup"
+    let c1 = Config 2 100 3
+        c2 = Config 3 80 5
+    print (c1 <> c2)
 
