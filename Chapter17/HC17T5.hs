32c08e72a0143df5f7f257027dad1ae2e2f1018b
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 13:53:13 +0200

    Update HC17T5.hs

--- Chapter17/HC17T5.hs
+++ Chapter17/HC17T5.hs
@@ -1,1 +1,9 @@
+-- HC17T5: combineLists Function
+combineLists :: [Int] -> [Int] -> [Int]
+combineLists xs ys = xs <> ys  -- Uses Semigroup (list concatenation)
+
+main :: IO ()
+main = do
+    putStrLn "HC17T5: combineLists"
+    print (combineLists [1,2,3] [4,5,6])  -- [1,2,3,4,5,6]
 
