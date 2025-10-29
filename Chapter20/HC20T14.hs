1d7a20e047964d03338a3b9522b2e5a897aa35fe
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:38:34 +0200

    Update HC20T14.hs

--- Chapter20/HC20T14.hs
+++ Chapter20/HC20T14.hs
@@ -1,1 +1,12 @@
+module Main where
+
+mapMFilter :: Monad m => (a -> m (Maybe b)) -> [a] -> m [b]
+mapMFilter _ [] = return []
+mapMFilter f (x:xs) = do
+  mx <- f x
+  rest <- mapMFilter f xs
+  return (maybe rest (:rest) mx)
+
+main :: IO ()
+main = print =<< mapMFilter (\x -> return (if even x then Just x else Nothing)) [1..10]
 
