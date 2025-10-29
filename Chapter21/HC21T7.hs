dde2b3085a5e09b1aaea6b03ff7806921999f111
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 17:08:08 +0200

    Update HC21T7.hs

--- Chapter21/HC21T7.hs
+++ Chapter21/HC21T7.hs
@@ -1,10 +1,38 @@
 module Main where
+import qualified Data.Map as M
 
-import Control.Monad (when)
+type Env = M.Map String String
 
-conditionalPrint :: Bool -> String -> IO ()
-conditionalPrint cond msg = when cond (putStrLn msg)
+-- Explicit version
+getPortExplicit :: Env -> String
+getPortExplicit env = "Port=" ++ M.findWithDefault "Unknown" "DB_PORT" env
+
+-- Reader abstraction
+newtype Reader r a = Reader { runReader :: r -> a }
+
+instance Functor (Reader r) where
+  fmap f (Reader g) = Reader (f . g)
+instance Applicative (Reader r) where
+  pure a = Reader (\_ -> a)
+  Reader f <*> Reader g = Reader (\r -> f r (g r))
+instance Monad (Reader r) where
+  return = pure
+  Reader g >>= f = Reader (\r -> runReader (f (g r)) r)
+
+ask :: Reader r r
+ask = Reader id
+
+local :: (r -> r) -> Reader r a -> Reader r a
+local f (Reader g) = Reader (g . f)
+
+getPort :: Reader Env String
+getPort = do
+  env <- ask
+  return ("Port=" ++ M.findWithDefault "Unknown" "DB_PORT" env)
 
 main :: IO ()
-main = conditionalPrint True "Condition is True!"
+main = do
+  let baseEnv = M.fromList [("DB_PORT","5432"),("DB_HOST","localhost")]
+  putStrLn (runReader getPort baseEnv)
+  putStrLn (runReader (local (M.insert "DB_PORT" "9999") getPort) baseEnv)
 
