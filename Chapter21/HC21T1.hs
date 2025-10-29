167fbaa6e67f4d0e71f8dcff976c4200cd499bc7
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 17:02:25 +0200

    Update HC21T1.hs

--- Chapter21/HC21T1.hs
+++ Chapter21/HC21T1.hs
@@ -1,1 +1,22 @@
+module Main where
+
+newtype Writer w a = Writer { runWriter :: (a, w) }
+
+tell :: Monoid w => w -> Writer w ()
+tell w = Writer ((), w)
+
+addW, subW, mulW :: Int -> Int -> Writer [String] Int
+addW x y = Writer (x + y, ["Added " ++ show x ++ " + " ++ show y])
+subW x y = Writer (x - y, ["Subtracted " ++ show x ++ " - " ++ show y])
+mulW x y = Writer (x * y, ["Multiplied " ++ show x ++ " * " ++ show y])
+
+calcDemo :: Writer [String] Int
+calcDemo =
+  let Writer (a, log1) = addW 3 5
+      Writer (b, log2) = subW a 2
+      Writer (c, log3) = mulW b 4
+  in Writer (c, log1 ++ log2 ++ log3)
+
+main :: IO ()
+main = print (runWriter calcDemo)
 
