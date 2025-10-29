91bf7e544b1a4878a9322780f289ca7ebd810092
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:49:32 +0200

    Update HC20T20.hs

--- Chapter20/HC20T20.hs
+++ Chapter20/HC20T20.hs
@@ -1,1 +1,11 @@
+module Main where
+
+batchProcessing :: IO ()
+batchProcessing =
+  getLine >>= \name ->
+  putStrLn ("Hello, " ++ name) >>
+  putStrLn "Processing complete."
+
+main :: IO ()
+main = batchProcessing
 
