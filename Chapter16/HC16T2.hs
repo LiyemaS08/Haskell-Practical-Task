59d9c0ca61602e873a393c1bc8175f29bf173c56
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 11:10:39 +0200

    Update HC16T2.hs

--- Chapter16/HC16T2.hs
+++ Chapter16/HC16T2.hs
@@ -1,1 +1,10 @@
+-- HC16T2: Palindrome Checker
+isPalindrome :: String -> Bool
+isPalindrome str = str == reverse str
+
+main :: IO ()
+main = do
+    putStrLn "HC16T2: Palindrome Checker"
+    print (isPalindrome "madam")
+    print (isPalindrome "hello")
 
