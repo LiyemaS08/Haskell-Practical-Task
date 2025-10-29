a01c1cd9fbe9c219b5f2cec53008e03b3c4605ad
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:16:13 +0200

    Update HC21T12.hs

--- Chapter21/HC21T12.hs
+++ Chapter21/HC21T12.hs
@@ -1,1 +1,8 @@
+module Main where
+
+sumThreeApplicative :: Either String Int -> Either String Int -> Either String Int -> Either String Int
+sumThreeApplicative a b c = (\x y z -> x + y + z) <$> a <*> b <*> c
+
+main :: IO ()
+main = print (sumThreeApplicative (Right 2) (Right 3) (Right 4))
 
