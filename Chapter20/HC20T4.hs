bfb286c6c3d68cb6f7ac11284548b792604ca919
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:33:18 +0200

    Update HC20T4.hs

--- Chapter20/HC20T4.hs
+++ Chapter20/HC20T4.hs
@@ -1,1 +1,13 @@
+module Main where
+
+import Control.Monad.State
+
+countChars :: Char -> String -> State Int ()
+countChars _ [] = return ()
+countChars c (x:xs)
+  | c == x    = modify (+1) >> countChars c xs
+  | otherwise = countChars c xs
+
+main :: IO ()
+main = print $ execState (countChars 'a' "bananas") 0
 
