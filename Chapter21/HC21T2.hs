22da0bab10f39553364f5b9c955814cab0faa63a
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 17:03:12 +0200

    Update HC21T2.hs

--- Chapter21/HC21T2.hs
+++ Chapter21/HC21T2.hs
@@ -1,8 +1,31 @@
 module Main where
+import Data.Monoid
 
-addThreeApplicative :: Maybe Int -> Maybe Int -> Maybe Int -> Maybe Int
-addThreeApplicative a b c = (\x y z -> x + y + z) <$> a <*> b <*> c
+newtype Writer w a = Writer { runWriter :: (a, w) }
+
+instance Functor (Writer w) where
+  fmap f (Writer (a, w)) = Writer (f a, w)
+
+instance Monoid w => Applicative (Writer w) where
+  pure a = Writer (a, mempty)
+  Writer (f, w1) <*> Writer (a, w2) = Writer (f a, w1 <> w2)
+
+instance Monoid w => Monad (Writer w) where
+  return = pure
+  Writer (a, w1) >>= f =
+    let Writer (b, w2) = f a
+    in Writer (b, w1 <> w2)
+
+tell :: Monoid w => w -> Writer w ()
+tell w = Writer ((), w)
+
+-- quick law check
+lawAssoc :: (Eq a, Monoid w) => Writer w a -> (a -> Writer w a) -> (a -> Writer w a) -> Bool
+lawAssoc m k h = runWriter ((m >>= k) >>= h) == runWriter (m >>= (\x -> k x >>= h))
 
 main :: IO ()
-main = print (addThreeApplicative (Just 2) (Just 3) (Just 4))
-
+main = do
+  let m = Writer (5, ["start"])
+      k x = Writer (x + 1, ["+1"])
+      h x = Writer (x * 2, ["*2"])
+  print (lawAssoc m k h)
