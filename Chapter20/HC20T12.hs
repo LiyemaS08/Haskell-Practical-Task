82b5ffe2cdbf5c8e05a6354668ffc2eb4c4bc537
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:37:51 +0200

    Update HC20T12.hs

--- Chapter20/HC20T12.hs
+++ Chapter20/HC20T12.hs
@@ -1,1 +1,10 @@
+module Main where
+
+fileRead :: FilePath -> IO ()
+fileRead path = do
+  contents <- readFile path
+  mapM_ putStrLn (lines contents)
+
+main :: IO ()
+main = fileRead "example.txt"
 
