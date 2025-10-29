c3e89134435a029a02a4905fb83bb29b21583ef7
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:49:17 +0200

    Update HC20T19.hs

--- Chapter20/HC20T19.hs
+++ Chapter20/HC20T19.hs
@@ -1,1 +1,19 @@
+module Main where
+
+import Control.Monad.Writer
+
+logFunction :: (Show a) => String -> a -> Writer [String] a
+logFunction name x = tell ["Called " ++ name ++ " with " ++ show x] >> return x
+
+systemProcess :: Writer [String] Int
+systemProcess = do
+  a <- logFunction "step1" 10
+  b <- logFunction "step2" (a * 2)
+  return b
+
+main :: IO ()
+main = do
+  let (res, logs) = runWriter systemProcess
+  print res
+  mapM_ putStrLn logs
 
