599de21f8b5f8764499dac481757f1ec85554ada
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 11:24:37 +0200

    Update HC16T8.hs

--- Chapter16/HC16T8.hs
+++ Chapter16/HC16T8.hs
@@ -1,1 +1,15 @@
+-- HC16T8: Insertion Sort
+insertionSort :: Ord a => [a] -> [a]
+insertionSort [] = []
+insertionSort (x:xs) = insert x (insertionSort xs)
+  where
+    insert y [] = [y]
+    insert y (z:zs)
+      | y <= z    = y : z : zs
+      | otherwise = z : insert y zs
+
+main :: IO ()
+main = do
+    putStrLn "HC16T8: Insertion Sort"
+    print (insertionSort [3,1,4,1,5,9,2,6])
 
