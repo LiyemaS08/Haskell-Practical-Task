5a38f64977fe17d054d6d181f00d7a84f9e4b8e8
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 11:11:29 +0200

    Update HC16T3.hs

--- Chapter16/HC16T3.hs
+++ Chapter16/HC16T3.hs
@@ -1,1 +1,10 @@
+-- HC16T3: Factorial
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+main :: IO ()
+main = do
+    putStrLn "HC16T3: Factorial"
+    print (factorial 5)
 
