cff126ea50dfbf0be05a73fe4193de1cd067ca8a
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 13:52:47 +0200

    Update HC17T4.hs

--- Chapter17/HC17T4.hs
+++ Chapter17/HC17T4.hs
@@ -1,1 +1,15 @@
+-- HC17T4: Monoid Instance for Sum
+newtype Sum = Sum { getSum :: Int } deriving (Show, Eq)
+
+instance Semigroup Sum where
+    (Sum x) <> (Sum y) = Sum (x + y)
+
+instance Monoid Sum where
+    mempty = Sum 0
+
+main :: IO ()
+main = do
+    putStrLn "HC17T4: Monoid for Sum"
+    print (mconcat [Sum 1, Sum 2, Sum 3])  -- Sum 6
+    print (mempty <> Sum 10)                -- Sum 10
 
