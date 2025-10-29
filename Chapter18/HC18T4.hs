dba1fceb16b96ec32b12911ba0569a90953ed208
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 14:01:13 +0200

    Update HC18T4.hs

--- Chapter18/HC18T4.hs
+++ Chapter18/HC18T4.hs
@@ -1,1 +1,9 @@
+-- HC18T4: mapToBits Function
+mapToBits :: [Bool] -> [Char]
+mapToBits = fmap (\b -> if b then '1' else '0')
+
+main :: IO ()
+main = do
+    putStrLn "HC18T4: mapToBits"
+    print (mapToBits [True, False, True, True])  -- "1011"
 
