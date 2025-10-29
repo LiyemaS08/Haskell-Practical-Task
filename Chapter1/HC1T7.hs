c8f766964170f8b971a273985d3b1b1de40f4a0d
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 17:26:27 +0200

    Update HC1T7.hs

--- Chapter1/HC1T7.hs
+++ Chapter1/HC1T7.hs
@@ -1,1 +1,10 @@
+-- Define the function
+fToC :: Floating a => a -> a
+fToC f = (f - 32) * 5 / 9
+
+-- Use it in main
+main :: IO ()
+main = do
+    print $ fToC 212   -- Should print 100.0
+    print $ fToC 32    -- Should print 0.0
 
