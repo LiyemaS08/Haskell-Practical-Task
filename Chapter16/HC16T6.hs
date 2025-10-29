a28dfd6bc7450f8bc7918e94ab9aa8abc041a71b
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 11:12:58 +0200

    Update HC16T6.hs

--- Chapter16/HC16T6.hs
+++ Chapter16/HC16T6.hs
@@ -1,1 +1,11 @@
+-- HC16T6: nth Fibonacci Number
+fibonacci :: Int -> Integer
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
+
+main :: IO ()
+main = do
+    putStrLn "HC16T6: nth Fibonacci Number"
+    print (fibonacci 10)
 
