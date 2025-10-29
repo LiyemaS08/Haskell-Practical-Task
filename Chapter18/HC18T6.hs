6abfc246e7f7b087d042730cf36c26d6a56defe3
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 14:03:46 +0200

    Update HC18T6.hs

--- Chapter18/HC18T6.hs
+++ Chapter18/HC18T6.hs
@@ -1,1 +1,10 @@
+-- HC18T6: applyToMaybe Function
+applyToMaybe :: (a -> b) -> Maybe a -> Maybe b
+applyToMaybe f = fmap f
+
+main :: IO ()
+main = do
+    putStrLn "HC18T6: applyToMaybe"
+    print (applyToMaybe (*2) (Just 10))  -- Just 20
+    print (applyToMaybe (*2) Nothing)    -- Nothing
 
