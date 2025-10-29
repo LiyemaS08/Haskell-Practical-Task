058fdee1a484265a7a29450d74de2263acd248b7
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 11:13:39 +0200

    Update HC16T7.hs

--- Chapter16/HC16T7.hs
+++ Chapter16/HC16T7.hs
@@ -1,1 +1,10 @@
+-- HC16T7: Element Existence in List
+elementExists :: (Eq a) => a -> [a] -> Bool
+elementExists x xs = x `elem` xs
+
+main :: IO ()
+main = do
+    putStrLn "HC16T7: Element Existence in List"
+    print (elementExists 3 [1,2,3,4,5])
+    print (elementExists 9 [1,2,3,4,5])
 
