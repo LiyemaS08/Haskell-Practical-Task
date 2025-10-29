c137c94446edaf37b3c127e3fc2405b13549a97c
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:21:34 +0200

    Update HC19T20.hs

--- Chapter19/HC19T20.hs
+++ Chapter19/HC19T20.hs
@@ -1,1 +1,10 @@
+module Main where
+
+import Control.Monad (forever)
+
+replicateForever :: IO () -> IO ()
+replicateForever = forever
+
+main :: IO ()
+main = replicateForever (putStrLn "Running forever...")  -- Ctrl+C to stop
 
