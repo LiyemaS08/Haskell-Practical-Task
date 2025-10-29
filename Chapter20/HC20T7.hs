3f6bf74c4263f96113ef9679b70c66f200c564af
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:34:01 +0200

    Update HC20T7.hs

--- Chapter20/HC20T7.hs
+++ Chapter20/HC20T7.hs
@@ -1,1 +1,13 @@
+module Main where
+
+findFirst :: (a -> Bool) -> [a] -> Either String a
+findFirst _ [] = Left "No match found"
+findFirst p (x:xs)
+  | p x       = Right x
+  | otherwise = findFirst p xs
+
+main :: IO ()
+main = do
+  print (findFirst even [1,3,4,5])
+  print (findFirst (>10) [1,2,3])
 
