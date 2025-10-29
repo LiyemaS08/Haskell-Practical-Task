d3b2deff732aa810f23044504fa4ef4c52ea79e7
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:33:29 +0200

    Update HC20T5.hs

--- Chapter20/HC20T5.hs
+++ Chapter20/HC20T5.hs
@@ -1,1 +1,14 @@
+module Main where
+
+import Control.Monad.Reader
+
+type Config = String
+
+greet :: Reader Config String
+greet = do
+  name <- ask
+  return ("Hello, " ++ name ++ "!")
+
+main :: IO ()
+main = print (runReader greet "Alice")
 
