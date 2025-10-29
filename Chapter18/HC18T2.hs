a286eebf8f06d11c98b6950beab4112450d98115
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 14:00:18 +0200

    Update HC18T2.hs

--- Chapter18/HC18T2.hs
+++ Chapter18/HC18T2.hs
@@ -1,1 +1,14 @@
+-- HC18T2: Functor Instance for Tree
+data Tree a = Leaf a | Node (Tree a) (Tree a)
+  deriving (Show)
+
+instance Functor Tree where
+    fmap f (Leaf x)   = Leaf (f x)
+    fmap f (Node l r) = Node (fmap f l) (fmap f r)
+
+main :: IO ()
+main = do
+    putStrLn "HC18T2: Functor Instance for Tree"
+    let t = Node (Leaf 1) (Node (Leaf 2) (Leaf 3))
+    print (fmap (*2) t)  -- Node (Leaf 2) (Node (Leaf 4) (Leaf 6))
 
