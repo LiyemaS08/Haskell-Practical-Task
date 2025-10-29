2ce7196c4149aca0480f6626054b8f7fae3b7d80
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 11:12:15 +0200

    Update HC16T5.hs

--- Chapter16/HC16T5.hs
+++ Chapter16/HC16T5.hs
@@ -1,1 +1,11 @@
+-- HC16T5: Uppercase String
+import Data.Char (toUpper)
+
+toUpperCase :: String -> String
+toUpperCase str = map toUpper str
+
+main :: IO ()
+main = do
+    putStrLn "HC16T5: Uppercase String"
+    print (toUpperCase "functional programming")
 
