96bc0529b375616958c1b78bc43a9028bd5d24cd
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 14:08:02 +0200

    Update HC18T9.hs

--- Chapter18/HC18T9.hs
+++ Chapter18/HC18T9.hs
@@ -1,1 +1,10 @@
+-- HC18T9: compositionLawCheck Function
+compositionLawCheck :: (Eq (f c), Functor f) => (b -> c) -> (a -> b) -> f a -> Bool
+compositionLawCheck f g x = fmap (f . g) x == (fmap f . fmap g) x
+
+main :: IO ()
+main = do
+    putStrLn "HC18T9: compositionLawCheck"
+    print (compositionLawCheck (*2) (+1) (Just 3))   -- True
+    print (compositionLawCheck show (*3) [1,2,3])    -- True
 
