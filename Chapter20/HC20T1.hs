c6f2eb37552fca3d658ee0fd15b59829a7dcbeae
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:31:58 +0200

    Update HC20T1.hs

--- Chapter20/HC20T1.hs
+++ Chapter20/HC20T1.hs
@@ -1,1 +1,11 @@
+module Main where
+
+safeDivide :: Int -> Int -> Maybe Int
+safeDivide _ 0 = Nothing
+safeDivide x y = Just (x `div` y)
+
+main :: IO ()
+main = do
+  print (safeDivide 10 2)
+  print (safeDivide 10 0)
 
