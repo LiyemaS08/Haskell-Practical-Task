654a0d45460aa08df09511c4c64686a5fcb8152f
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:25:44 +0200

    Update HC19T13.hs

--- Chapter19/HC19T13.hs
+++ Chapter19/HC19T13.hs
@@ -1,1 +1,10 @@
+module Main where
+
+import Control.Monad (when)
+
+whenApplicative :: Bool -> IO () -> IO ()
+whenApplicative cond action = when cond action
+
+main :: IO ()
+main = whenApplicative True (putStrLn "Running whenApplicative!")
 
