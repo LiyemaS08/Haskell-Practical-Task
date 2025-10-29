c8bb59f53a1408dfc859a385dfc651e3c410f473
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 17:26:46 +0200

    Update HC1T8.hs

--- Chapter1/HC1T8.hs
+++ Chapter1/HC1T8.hs
@@ -1,1 +1,8 @@
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+main :: IO ()
+main = do
+    print $ applyTwice (+3) 10           -- Output: 16
+    print $ applyTwice (++ "ha") "Hey"  -- Output: "Heyhaha"
 
