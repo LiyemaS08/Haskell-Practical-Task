ae752d3b430bd3195a3d2226fbbf9ca121ea7e92
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 11:11:52 +0200

    Update HC16T4.hs

--- Chapter16/HC16T4.hs
+++ Chapter16/HC16T4.hs
@@ -1,1 +1,9 @@
+-- HC16T4: Filter Even Numbers
+filterEven :: [Int] -> [Int]
+filterEven xs = filter even xs
+
+main :: IO ()
+main = do
+    putStrLn "HC16T4: Filter Even Numbers"
+    print (filterEven [1..10])
 
