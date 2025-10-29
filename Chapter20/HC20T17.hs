45702050f15af6ee4182f3d7edd045290faba05e
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:48:52 +0200

    Update HC20T17.hs

--- Chapter20/HC20T17.hs
+++ Chapter20/HC20T17.hs
@@ -1,1 +1,13 @@
+module Main where
+
+validatePassword :: String -> Either String String
+validatePassword pwd
+  | length pwd < 8 = Left "Password too short"
+  | not (any (`elem` ['0'..'9']) pwd) = Left "Must include a digit"
+  | otherwise = Right "Password valid"
+
+main :: IO ()
+main = do
+  print (validatePassword "abc")
+  print (validatePassword "pass1234")
 
