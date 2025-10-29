f215b5d9aa8382e93a5dd5d73da0e278a73f5427
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 17:08:32 +0200

    Update HC21T8.hs

--- Chapter21/HC21T8.hs
+++ Chapter21/HC21T8.hs
@@ -1,8 +1,41 @@
 module Main where
 
-discardSecond :: Applicative f => f a -> f b -> f a
-discardSecond = (<*)
+-- Basic State type
+newtype State s a = MkState { runState :: s -> (a, s) }
+
+instance Functor (State s) where
+  fmap f (MkState g) = MkState (\s -> let (a,s') = g s in (f a, s'))
+
+instance Applicative (State s) where
+  pure a = MkState (\s -> (a,s))
+  MkState f <*> MkState g = MkState (\s ->
+    let (func,s1) = f s
+        (a,s2) = g s1
+    in (func a, s2))
+
+instance Monad (State s) where
+  return = pure
+  MkState g >>= f = MkState (\s ->
+    let (a,s1) = g s
+        MkState h = f a
+    in h s1)
+
+-- State helpers
+get :: State s s
+get = MkState (\s -> (s,s))
+
+put :: s -> State s ()
+put s = MkState (\_ -> ((),s))
+
+modify :: (s -> s) -> State s ()
+modify f = MkState (\s -> ((), f s))
+
+-- Example
+counter :: State Int String
+counter = do
+  modify (+1)
+  x <- get
+  return ("Count is now " ++ show x)
 
 main :: IO ()
-main = print (discardSecond (Just 5) (Just 10))
-
+main = print (runState counter 0)
