ee202200b4a009a9e70a8a1508e6db8fee8962b5
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:49:06 +0200

    Update HC20T18.hs

--- Chapter20/HC20T18.hs
+++ Chapter20/HC20T18.hs
@@ -1,1 +1,18 @@
+module Main where
+
+import Control.Monad.Trans.Maybe
+import Control.Monad.IO.Class
+
+getValidatedInput :: MaybeT IO String
+getValidatedInput = do
+  liftIO (putStrLn "Enter a non-empty name:")
+  name <- liftIO getLine
+  if null name then MaybeT (return Nothing) else return name
+
+main :: IO ()
+main = do
+  result <- runMaybeT getValidatedInput
+  case result of
+    Just name -> putStrLn ("Hello, " ++ name)
+    Nothing   -> putStrLn "Invalid input!"
 
