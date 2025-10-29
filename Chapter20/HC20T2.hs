50851ab52885f10195b6744adf8bd84ed3c9ed52
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:32:14 +0200

    Update HC20T2.hs

--- Chapter20/HC20T2.hs
+++ Chapter20/HC20T2.hs
@@ -1,1 +1,10 @@
+module Main where
+
+sequenceMaybe :: [Maybe a] -> Maybe [a]
+sequenceMaybe = sequenceA
+
+main :: IO ()
+main = do
+  print (sequenceMaybe [Just 1, Just 2, Just 3])
+  print (sequenceMaybe [Just 1, Nothing, Just 3])
 
