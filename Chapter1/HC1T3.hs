864362bc31f1b5f7bce10e187fa7e6a406a85dca
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 17:24:54 +0200

    Update HC1T3.hs

--- Chapter1/HC1T3.hs
+++ Chapter1/HC1T3.hs
@@ -1,1 +1,8 @@
+greaterThan18 :: (Ord a, Num a) => a -> Bool
+greaterThan18 n = n > 18
+
+main :: IO ()
+main = do
+    let testNumber = 20
+    putStrLn ("Is " ++ show testNumber ++ " greater than 18? " ++ show (greaterThan18 testNumber))
 
