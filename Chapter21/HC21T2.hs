7c0305b47b1901f884923ce69bf13daf4b11aa6f
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:10:48 +0200

    Update HC21T2.hs

--- Chapter21/HC21T2.hs
+++ Chapter21/HC21T2.hs
@@ -1,1 +1,8 @@
+module Main where
+
+addThreeApplicative :: Maybe Int -> Maybe Int -> Maybe Int -> Maybe Int
+addThreeApplicative a b c = (\x y z -> x + y + z) <$> a <*> b <*> c
+
+main :: IO ()
+main = print (addThreeApplicative (Just 2) (Just 3) (Just 4))
 
