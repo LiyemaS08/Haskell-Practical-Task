95c565b164daa5682102f1aa19a0fdd3acf7c0e4
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 17:11:37 +0200

    Update HC21T11.hs

--- Chapter21/HC21T11.hs
+++ Chapter21/HC21T11.hs
@@ -1,14 +1,43 @@
 module Main where
 
-data Wrapper a = Wrapper a deriving (Show, Eq)
+newtype State s a = MkState { runState :: s -> (a, s) }
 
-instance Functor Wrapper where
-  fmap f (Wrapper x) = Wrapper (f x)
+instance Functor (State s) where
+  fmap f (MkState g) = MkState (\s -> let (a,s')=g s in (f a,s'))
+instance Applicative (State s) where
+  pure a = MkState (\s -> (a,s))
+  MkState f <*> MkState g = MkState (\s ->
+    let (func,s1)=f s; (a,s2)=g s1 in (func a,s2))
+instance Monad (State s) where
+  return=pure
+  MkState g >>= f = MkState (\s -> let (a,s1)=g s; MkState h=f a in h s1)
 
-instance Applicative Wrapper where
-  pure = Wrapper
-  Wrapper f <*> Wrapper x = Wrapper (f x)
+get :: State s s
+get = MkState (\s -> (s,s))
+put :: s -> State s ()
+put s = MkState (\_ -> ((),s))
+
+-- State (current, history)
+setValue :: Int -> State (Int,[Int]) ()
+setValue new = do
+  (curr, hist) <- get
+  put (new, curr:hist)
+
+undo :: State (Int,[Int]) ()
+undo = do
+  (curr, hist) <- get
+  case hist of
+    [] -> return ()
+    (h:rest) -> put (h, rest)
+
+script :: State (Int,[Int]) Int
+script = do
+  setValue 10
+  setValue 20
+  undo
+  setValue 30
+  (curr, _) <- get
+  return curr
 
 main :: IO ()
-main = print (Wrapper (+1) <*> Wrapper 5)
-
+main = print (runState script (0,[]))
