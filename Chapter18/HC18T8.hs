aafd1e1dbea770b02a401adc61ffb8bbf31781f3
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 14:06:02 +0200

    Update HC18T8.hs

--- Chapter18/HC18T8.hs
+++ Chapter18/HC18T8.hs
@@ -1,1 +1,10 @@
+-- HC18T8: identityLawCheck Function
+identityLawCheck :: (Eq (f a), Functor f) => f a -> Bool
+identityLawCheck x = fmap id x == x
+
+main :: IO ()
+main = do
+    putStrLn "HC18T8: identityLawCheck"
+    print (identityLawCheck (Just 5))        -- True
+    print (identityLawCheck [1,2,3])         -- True
 
