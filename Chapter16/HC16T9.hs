d41c95512e9e21cf1759ad27022d017006a457e8
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 11:27:26 +0200

    Update HC16T9.hs

--- Chapter16/HC16T9.hs
+++ Chapter16/HC16T9.hs
@@ -1,1 +1,11 @@
+-- HC16T9: Remove Duplicates from List
+import Data.List (nub)
+
+removeDuplicates :: Eq a => [a] -> [a]
+removeDuplicates xs = nub xs
+
+main :: IO ()
+main = do
+    putStrLn "HC16T9: Remove Duplicates from List"
+    print (removeDuplicates [1,2,2,3,3,4,4,4])
 
