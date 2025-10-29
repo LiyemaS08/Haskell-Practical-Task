f625060260ce47ee98663447b3f5bf01ebe79276
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-29 16:26:11 +0200

    Update HC19T14.hs

--- Chapter19/HC19T14.hs
+++ Chapter19/HC19T14.hs
@@ -1,1 +1,12 @@
+module Main where
+
+import Control.Monad (replicateM)
+
+replicateEffect :: Int -> IO a -> IO [a]
+replicateEffect n action = replicateM n action
+
+main :: IO ()
+main = do
+  xs <- replicateEffect 3 (putStrLn "Hi" >> return 42)
+  print xs
 
