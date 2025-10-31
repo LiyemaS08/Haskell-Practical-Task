6f1a86b1a0daa40026c3a5bc92069c3100277139
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-31 08:48:40 +0200

    Update HC14T7.hs

--- Chapter14/HC14T7.hs
+++ Chapter14/HC14T7.hs
@@ -1,6 +1,13 @@
-library
-  exposed-modules: Utils
-  hs-source-dirs: src
-  build-depends: base >=4.14 && <5
-  default-language: Haskell2010
+-- Main.hs
 
+-- Library-style functions
+double :: Num a => a -> a
+double x = x * 2
+
+triple :: Num a => a -> a
+triple x = x * 3
+
+main :: IO ()
+main = do
+    putStrLn ("Double 5 = " ++ show (double 5))
+    putStrLn ("Triple 5 = " ++ show (triple 5))
