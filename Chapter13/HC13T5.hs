c9f415466a08795965285ce8be7a30fca2646b42
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-31 08:20:19 +0200

    Update HC13T5.hs

--- Chapter13/HC13T5.hs
+++ Chapter13/HC13T5.hs
@@ -1,11 +1,16 @@
--- HC13T5 - Restricted Export
-module SumNonEmpty (sumNonEmpty) where
+-- Main.hs
 
-sumNonEmpty :: [Int] -> Int
-sumNonEmpty [] = errorMsg
+-- SumNonEmpty function
+sumNonEmpty :: Num a => [a] -> a
+sumNonEmpty [] = error "sumNonEmpty: empty list"
 sumNonEmpty xs = sum xs
 
--- private helper (not exported)
-errorMsg :: String
-errorMsg = error "List cannot be empty!"
+main :: IO ()
+main = do
+    let list1 = [1,2,3,4]
+    putStrLn $ "Sum of list1: " ++ show (sumNonEmpty list1)
 
+    let list2 = []          -- OK now, because we do something with it next
+    putStrLn "Trying to sum list2..."
+    -- Uncomment the next line to see the error on empty list
+    -- putStrLn $ "Sum of list2: " ++ show (sumNonEmpty list2)
