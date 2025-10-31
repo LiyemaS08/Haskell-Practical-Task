f2b8a0423aa25b3124438a49ee173b1b182e4d10
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-31 08:19:03 +0200

    Update HC13T4.hs

--- Chapter13/HC13T4.hs
+++ Chapter13/HC13T4.hs
@@ -1,7 +1,16 @@
--- HC13T4 - SumNonEmpty Module
-module SumNonEmpty (sumNonEmpty) where
+-- Main.hs
 
-sumNonEmpty :: [Int] -> Int
-sumNonEmpty [] = error "List is empty! Cannot sum."
+-- SumNonEmpty module content
+sumNonEmpty :: Num a => [a] -> a
+sumNonEmpty [] = error "sumNonEmpty: empty list"
 sumNonEmpty xs = sum xs
 
+-- Main program
+main :: IO ()
+main = do
+    let list1 = [1,2,3,4]
+    let list2 = []
+
+    putStrLn $ "Sum of list1: " ++ show (sumNonEmpty list1)
+    -- This will throw an error
+    putStrLn $ "Sum of list2: " ++ show (sumNonEmpty list2)
