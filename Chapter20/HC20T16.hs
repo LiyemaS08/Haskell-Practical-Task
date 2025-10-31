bedf3739b1fc5b269a81adeff6cb2d26172b7255
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-31 10:08:23 +0200

    Update HC20T16.hs

--- Chapter20/HC20T16.hs
+++ Chapter20/HC20T16.hs
@@ -1,17 +1,21 @@
-module Main where
+-- HC20T16.hs
 
+import Control.Exception (try, SomeException)
+
+-- retryIO: retries an IO action up to n times
 retryIO :: Int -> IO a -> IO (Maybe a)
-retryIO 0 _ = return Nothing
+retryIO 0 _ = return Nothing  -- no attempts left
 retryIO n action = do
-  result <- (Just <$> action) `catch` (\(_ :: IOError) -> return Nothing)
-  case result of
-    Just v  -> return (Just v)
-    Nothing -> retryIO (n - 1) action
+    result <- try action  -- catch any exception
+    case result of
+        Right val -> return (Just val)  -- success
+        Left (_ :: SomeException) -> retryIO (n - 1) action  -- retry
 
-import Control.Exception (catch, IOException)
-
+-- Example usage
 main :: IO ()
 main = do
-  res <- retryIO 3 (readFile "missing.txt")
-  print (fmap (take 20) res)
-
+    putStrLn "Trying to read a file (example)..."
+    res <- retryIO 3 (readFile "nonexistent.txt")  -- replace with any IO action
+    case res of
+        Just content -> putStrLn $ "Success! Content length: " ++ show (length content)
+        Nothing      -> putStrLn "Failed after 3 attempts."
