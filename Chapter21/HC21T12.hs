a3839ba1222bf3d336a402e8fbeba57eefc750b3
LiyemaS08 <sibizoliyema@gmail.com>
2025-10-31 10:22:33 +0200

    Update HC21T12.hs

--- Chapter21/HC21T12.hs
+++ Chapter21/HC21T12.hs
@@ -1,40 +1,45 @@
-module Main where
+-- main.hs
 
-newtype State s a = MkState { runState :: s -> (a, s) }
+import Control.Monad.State
 
-instance Functor (State s) where
-  fmap f (MkState g) = MkState (\s -> let (a,s')=g s in (f a,s'))
-instance Applicative (State s) where
-  pure a = MkState (\s -> (a,s))
-  MkState f <*> MkState g = MkState (\s ->
-    let (func,s1)=f s; (a,s2)=g s1 in (func a,s2))
-instance Monad (State s) where
-  return=pure
-  MkState g >>= f = MkState (\s -> let (a,s1)=g s; MkState h=f a in h s1)
+type Position = (Int, Int)
+type RNGState = (Int, Position)  -- (seed, position)
 
--- randomStep: seed & position
-randomStep :: State (Int,(Int,Int)) ()
-randomStep = MkState $ \(seed,(x,y)) ->
-  let newSeed = (seed * 1103515245 + 12345) `mod` 256
-      dir = newSeed `mod` 4
-      (x',y') = case dir of
-        0 -> (x+1,y)
-        1 -> (x-1,y)
-        2 -> (x,y+1)
-        _ -> (x,y-1)
-  in ((), (newSeed,(x',y')))
+-- Simple pseudo-random number generator: linear congruential generator
+nextSeed :: Int -> Int
+nextSeed s = (s * 1103515245 + 12345) `mod` 2147483648
 
-randomWalk :: Int -> State (Int,(Int,Int)) [(Int,Int)]
-randomWalk 0 = do
-  (_,pos) <- MkState (\s -> (s,s))  -- identity
-  return [snd s | s <- [snd (runState (MkState id) (0,(0,0)))]]
+-- Take a number mod 4 to choose direction
+directionFromSeed :: Int -> (Int, Int)
+directionFromSeed s = case s `mod` 4 of
+    0 -> (0, 1)   -- Up
+    1 -> (0, -1)  -- Down
+    2 -> (-1, 0)  -- Left
+    _ -> (1, 0)   -- Right
+
+-- Random step: update seed and move position
+randomStep :: State RNGState ()
+randomStep = do
+    (seed, pos) <- get
+    let newSeed = nextSeed seed
+        move = directionFromSeed newSeed
+        newPos = (fst pos + fst move, snd pos + snd move)
+    put (newSeed, newPos)
+
+-- Random walk of n steps, including start
+randomWalk :: Int -> State RNGState [Position]
 randomWalk n = do
-  randomStep
-  MkState $ \(seed,pos) ->
-    let (path, st') = runState (randomWalk (n-1)) (seed,pos)
-    in (pos:path, st')
+    (_, startPos) <- get
+    let go 0 acc = return (reverse acc)
+        go k acc = do
+            randomStep
+            (_, pos) <- get
+            go (k-1) (pos:acc)
+    go n [startPos]
 
+-- Main to test
 main :: IO ()
 main = do
-  let (path, _) = runState (randomWalk 5) (42,(0,0))
-  print path
+    let startState = (42, (0,0))  -- seed 42, starting at (0,0)
+        path = evalState (randomWalk 10) startState
+    print path
